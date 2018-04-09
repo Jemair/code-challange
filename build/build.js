6b6ae549cdef514ca0fb183149d7d8c8bae7d622
J <wanglianming@bilibili.com>
2018-04-09 19:15:38 +0800

    init

---
+++ build/build.js
@@ -0,0 +1,46 @@
+// https://github.com/shelljs/shelljs
+require('./check-versions')()
+
+process.env.NODE_ENV = 'production'
+
+const chalkAnimation = require('chalk-animation')
+const path = require('path')
+const chalk = require('chalk')
+const shell = require('shelljs')
+const webpack = require('webpack')
+const config = require('../config')
+const webpackConfig = require('./webpack.prod.conf')
+
+const error = shell.exec('npm run lint')
+
+if (error.code) {
+  shell.exit(1)
+}
+
+const animation = chalkAnimation.rainbow('building for production...')
+animation.start()
+
+const assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
+shell.rm('-rf', assetsPath)
+shell.mkdir('-p', assetsPath)
+shell.config.silent = true
+shell.cp('-R', 'static/*', assetsPath)
+shell.config.silent = false
+
+webpack(webpackConfig, function (err, stats) {
+  animation.stop()
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n\n')
+
+  console.log(chalk.cyan('  Build complete.\n'))
+  console.log(chalk.yellow(
+    '  Tip: built files are meant to be served over an HTTP server.\n' +
+    '  Opening index.html over file:// won\'t work.\n'
+  ))
+})
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import { renderRoutes } from 'react-router-config'
+import { BrowserRouter as Router } from 'react-router-dom'
+import routes from 'routes'
+import './App.styl'
+
+const App = () => (
+  <div className="App">
+    <Router>
+      {renderRoutes(routes)}
+    </Router>
+  </div>
+)
+
+export default hot(module)(App)
